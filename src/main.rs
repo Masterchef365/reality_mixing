766f1bd8780822e46af7f71fc2d15bec7d89edfc
Masterchef365 <duncan.freeman1@gmail.com>
2021-04-29 20:26:41 -0700

    Working pointcloud! Sorta

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
-use anyhow::Result;
 use shortcuts::{
     launch,
-    mesh::*,
     shader,
     starter_kit::{self, StarterKit},
     memory::{self, ManagedBuffer},
-    FrameDataUbo, MultiPlatformCamera, StagingBuffer, Vertex,
+    FrameDataUbo, MultiPlatformCamera,
 };
 use watertender::*;
 
+use anyhow::{ensure, Result};
+use realsense_rust::{
+    config::Config,
+    context::Context,
+    frame::DepthFrame,
+    kind::{Rs2CameraInfo, Rs2Format, Rs2ProductLine, Rs2StreamKind},
+    pipeline::{InactivePipeline, ActivePipeline},
+};
+use std::{
+    collections::HashSet,
+    convert::TryFrom,
+    io::{self, Write},
+    time::Duration,
+};
+
+
 struct App {
     pointcloud_gpu: Vec<ManagedBuffer>,
+    depth_camera_pipeline: ActivePipeline,
 
     pipeline: vk::Pipeline,
     pipeline_layout: vk::PipelineLayout,
@@ -36,11 +51,30 @@ struct SceneData {
 unsafe impl bytemuck::Zeroable for SceneData {}
 unsafe impl bytemuck::Pod for SceneData {}
 
-const N_POINTS: usize = 1024;
-const POINTCLOUD_SIZE: usize = 1024 * 3 * 2 * std::mem::size_of::<f32>();
+const DEPTH_WIDTH: usize = 640;
+const DEPTH_HEIGHT: usize = 480;
+const N_POINTS: usize = DEPTH_WIDTH * DEPTH_HEIGHT;
+const POINTCLOUD_SIZE: usize = N_POINTS * 3 * 2 * std::mem::size_of::<f32>();
 
 impl MainLoop for App {
     fn new(core: &SharedCore, mut platform: Platform<'_>) -> Result<Self> {
+        // ############################## Realsense stuff ############################## 
+        let mut queried_devices = HashSet::new();
+        queried_devices.insert(Rs2ProductLine::Sr300);
+        let context = Context::new()?;
+        let devices = context.query_devices(queried_devices);
+        ensure!(!devices.is_empty(), "No devices found");
+
+        // create pipeline
+        let pipeline = InactivePipeline::try_from(&context)?;
+        let mut config = Config::new();
+        config
+            .enable_device_from_serial(devices[0].info(Rs2CameraInfo::SerialNumber).unwrap())?
+            .disable_all_streams()?
+            .enable_stream(Rs2StreamKind::Depth, None, 640, 480, Rs2Format::Z16, 60)?;
+        let mut depth_camera_pipeline = pipeline.start(Some(config))?;
+
+        // ############################## Vulkan graphics stuff ############################## 
         let starter_kit = StarterKit::new(core.clone(), &mut platform)?;
 
         // Camera
@@ -106,6 +140,7 @@ impl MainLoop for App {
         }).collect::<Result<_>>()?;
 
         Ok(Self {
+            depth_camera_pipeline,
             pointcloud_gpu,
             camera,
             anim: 0.0,
@@ -122,21 +157,37 @@ impl MainLoop for App {
         core: &SharedCore,
         platform: Platform<'_>,
     ) -> Result<PlatformReturn> {
+        // ############################## Realsense to Vulkan ############################## 
+        let timeout = Duration::from_millis(5000);
+        let frames = self.depth_camera_pipeline.wait(Some(timeout))?;
+        let mut depth_frames = frames.frames_of_type::<DepthFrame>();
+
+        if !depth_frames.is_empty() {
+            let depth_frame = depth_frames.pop().unwrap();
+            let size = depth_frame.get_data_size() / std::mem::size_of::<u16>();
+            //let stride = depth_frame.stride() / std::mem::size_of::<u16>();
+            let mut pts = Vec::with_capacity(POINTCLOUD_SIZE);
+            unsafe {
+                let data = depth_frame.get_data() as *const std::os::raw::c_void;
+                let slice = std::slice::from_raw_parts(data.cast::<u16>(), size);
+                for (row_idx, row) in slice.chunks_exact(DEPTH_WIDTH).enumerate() {
+                    for (col_idx, col) in row.iter().enumerate() {
+                        let x = col_idx as f32 / 100.;
+                        let y = row_idx as f32 / 100.;
+                        let z = *col as f32 / 100.;
+                        pts.extend_from_slice(&[x, y, z]);
+                        pts.extend_from_slice(&[1.; 3]);
+                    }
+                }
+            }
+            self.pointcloud_gpu[self.starter_kit.frame].write_bytes(0, bytemuck::cast_slice(&pts))?;
+        }
+
+
+
+        // ############################## Command buffer ############################## 
         let cmd = self.starter_kit.begin_command_buffer(frame)?;
         let command_buffer = cmd.command_buffer;
-
-        let mut pts = Vec::with_capacity(POINTCLOUD_SIZE);
-        for i in 0..N_POINTS {
-            let i = i as f32 / 100.;
-            let x = (self.anim + i).cos();
-            let y = (self.anim * i).sin();
-            let z = (self.anim - i).sin();
-            pts.extend_from_slice(&[x, y, z]);
-            let (r, g, b) = (x.abs(), y.abs(), z.abs());
-            pts.extend_from_slice(&[r, g, b]);
-        }
-        self.pointcloud_gpu[self.starter_kit.frame].write_bytes(0, bytemuck::cast_slice(&pts))?;
-
         unsafe {
             core.device.cmd_bind_descriptor_sets(
                 command_buffer,
